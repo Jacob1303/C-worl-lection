6c8c2f9bd4c552d8a4a59827d45c57b30a3f42d4
yakov kuzin <kuzin_js@mail.ru>
2022-08-06 13:49:58 +0300

    Example2.1.Added Random funct

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // любое число от 1 до 9, целое 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // любое число от 1 до 9, целое 
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
